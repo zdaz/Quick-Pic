fdd22a33de75d353ba4a6348bbf286b58151aaff
Zachary Dalzell <zdaz14@gmail.com>
2014-01-16 18:52:59 -0600

    Initial Commit

---
+++ Quick Pic/ZDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ZDAppDelegate.h
+//  Quick Pic
+//
+//  Created by Zach on 1/16/14.
+//  Copyright (c) 2014 Zach Dalzell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Quick Pic/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Quick Pic
+//
+//  Created by Zach on 1/16/14.
+//  Copyright (c) 2014 Zach Dalzell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ZDAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ZDAppDelegate class]));
+    }
+}
